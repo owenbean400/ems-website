07d3866adac6c8907dd64aa2cf30564485f8dd59
Owen Bean <owenbean@Owens-MacBook-Pro-98.local>
2023-08-05 15:42:42 -0400

    Added ELUA and updated privacy policy

---
+++ src/pages/elua.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import MetaData from "../components/Metadata";
+import "../style/markdown.sass";
+import { Link } from "gatsby";
+
+// markup
+const ELUA = () => {
+  const [phoneSize, setPhoneSize] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setPhoneSize(window.innerWidth <= 768);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div id="main">
+      <Navbar isPhone={phoneSize}></Navbar>
+      <MetaData title="Response Time Track - End User License Agreement" />
+      <main>
+        <h1>End User License Agreement</h1>
+        <h3>Introduction</h3>
+        <p>
+        PLEASE READ THIS END USER LICENSE AGREEMENT CAREFULLY. This End User License Agreement ("EULA") is a legally binding agreement between you (the "User") and Response Time Track governing your access to and use of the Response Time Track mobile applications, which are designed for use with Apple iOS and Google Android OS operating systems (the "Applications"). By downloading, installing, or using the Applications you agree to be bound by the terms of this EULA. Response Time Track reserves the right to change or modify any of the terms and conditions contained in this EULA without notice. All such revisions will be effective immediately upon posting and will apply to all access and continued use of the Applications.
+        </p>
+        <p>BY DOWNLOADING/INSTALLING/USING THE APPLICATIONS, YOU:</p>
+        <ol>
+            <li>Agree to all of the terms and conditions contained in this EULA;</li>
+            <li>Agree to all other policies maintained by Response Time Track, such as our <Link to="/privacyPolicy">Privacy Policy</Link>, which explains what information we collect from you;</li>
+            <li>Understand that if you access Response Time Track from outside permitted areas you are doing so at your own risk;</li>
+            <li>Understand that if you do not agree with any portion of this EULA, you must refrain from downloading, installing or using the Applications.</li>
+        </ol>
+        <h3>License</h3>
+        <p>Response Time Track grants you a non-transferable, non-sublicenseable, commercial and personal license to install and/or use the application in its entirety or its components, until either party terminates this EULA. This application is not sold to you but licensed to you.</p>
+        <ol>
+            <li>You shall not, directly or indirectly, exploit any part or the whole of the product, reverse engineer, decompile, disassemble, adapt or otherwise reproduce the software or create derivative works of it;</li>
+            <li>You shall not copy, modify, translate, adapt or otherwise create improvements (whether patentable or not) from the application;</li>
+            <li>You shall not directly or indirectly sell, rent, lease sublicense distribute publish transfer nor market any features of the application.</li>
+        </ol>
+        <h3>Ownership</h3>
+        <p>All title, ownership rights, and intellectual property rights in the Product and any copies thereof are owned by Response Time Track or its licensors. The Product is protected by national and international laws, copyright treaties, conventions, and other laws. Response Time Track may also assert its rights in the event of any violation of this Agreement. Any reproduction or representation of these licensed materials for any reason is prohibited without Response Time Track's prior permission. The license does not grant title nor ownership in the Product. <br></br>
+
+        We do not assert any ownership over the content produced through the App. You retain full ownership of all your content and any intellectual property rights associated with it. We are not responsible for the content produced through the Application, which remains yours alone. We do reserve the right to utilize data provided to us</p>
+        <h3>Updates</h3>
+        <p>Response Time Track may develop and provide application updates, which may include upgrades, bug fixes, patches, and other error corrections and/or new features. Updates may modify or delete in their entirely certain features or functinoality. You agree Response Time Track has no obiligation to provide any updates or to continue to provide or enable features and functionality. Depending on your mobile device setting, when your mobile device is connected to the internet: 1. The application will automatically download or install updates or 2. modify, translate, adapt or otherwise create derivative works or improvements, whether or not patentable of application 3. You may receive notice or be prompted to download and install available updates. <br></br>
+
+        You shall promptly download and install all updates and acknowledge and agree to that application or part of the application may not operate properly should you fail to do so. Furthermore, you agree that all updated are part of Response Time Track and be subject to all terms and conditions of this agreement.</p>
+        <h3>Terms of Termination</h3>
+        <p>The term of this Agreement commences when you download/install Response Time Track and will continue to stay in effect until terminated. You may terminate this Agreement by deleting the Application, unsubscribing to the Application, and removal of all data within Response Time Track. Response Time Track also reserves the right to terminate this Agreement at any time without prior notice if it ceases to support the Application. Furthermore, this Agreement will be immediately terminated without prior notice if you violate any of the terms and conditions of this Agreement. Upon termination, all rights and obligations under this Agreement shall also be terminated and you must cease all use of the Application.</p>
+        <h3>Disclaimer of Warranties</h3>
+        <p>YOU ACKNOWLEDGE AND AGREE THAT USE OF THE LICENSE IS AT YOUR OWN RISK. UNDER APPLICABLE LAW, THE PRODUCT IS SUPPLIED ON AN 'AS IS' AND 'AS AVAILABLE' BASIS. ELUA MAKES NO WARRANTIES OR GUARANTEES, EITHER EXPRESSED OR IMPLIED, CONCERNING: (A) ITS ACCURACY, CURRENTNESS, COMPLETENESS, RELIABILITY OR SECURITY; (B) ANY IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT; (C) ITS MARKET VALUE; OR (D) YOUR SATISFACTION. NO ORAL OR WRITTEN INFORMATION OR ADVICE PROVIDED BY ELUA OR ITS AUTHORIZED REPRESENTATIVE SHALL CREATE ANY WARRANTY.</p>
+        <h3>Limitation of Liability</h3>
+        <p>TO THE FULLEST EXTENT PERMITTED BY APPLICABLE LAW, ELUA AND ITS AFFILIATES, SERVICE PROVIDERS, OR LICENSORS SHALL NOT BE LIABLE FOR ANY PERSONAL INJURIES OR DAMAGES OF ANY INCIDENTAL, SPECIAL, INDIRECT, OR CONSEQUENTIAL NATURE WHATSOEVER, INCLUDING LOSSES OF PROFITS, DATA, BUSINESS INTERRUPTION, OR ANY OTHER COMMERCIAL DAMAGES OR LOSSES THAT MAY ARISE OUT OF OR ARE RELATED TO YOUR USE OF OR INABILITY TO USE THE APPLICATION. THIS LIMITATION APPLIES REGARDLESS OF THEORY OF LIABILITY AND CONTINUES EVEN IF ELUA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.</p>
+        <p>The Licensed Application and related documentation are "Commercial Items", as that term is defined at 48 C.F.R. §2.101, consisting of "Commercial Computer Software" and "Commercial Computer Software Documentation", as such terms are used in 48 C.F.R. §12.212 or 48 C.F.R. §227.7202, as applicable. Consistent with 48 C.F.R. §12.212 or 48 C.F.R. §227.7202-1 through 227.7202-4, as applicable, the Commercial Computer Software and Commercial Computer Software Documentation are being licensed to U.S. Government end users (a) only as Commercial Items and (b) with only those rights as are granted to all other end users pursuant to the terms and conditions herein. Unpublished-rights reserved under the copyright laws of the United States.</p>
+      </main>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default ELUA;
--- src/pages/privacyPolicy.js
+++ src/pages/privacyPolicy.js
@@ -27,39 +27,33 @@ const IndexPage = () => {
       <main>
         <h1>Privacy Policy</h1>
         <p>
-          Response Time Track app will not collect data in offline mode. The
-          times are saved on the hardware of the phone.
+          Responses vary in their data collection process, depending on the type of data provided. The manner in which the data is handled is contingent on its type.
         </p>
-        <h3>Offline Mode</h3>
+        <h3>Times</h3>
         <p className="doc-txt">
-          Response Time Track app will not collect any data in offline mode. The
-          data in offline mode stays on the phone hardware. We can't acess this
-          data on the hardware as long as it is not sent to the server. Once
-          login, the data can be sent to the server, which is under the user
-          mode.
+          Response Time Track app will not collect nor share any times in guest mode. Response Time Track does not accept any responsibility for any data lost in guest mode and does not have any backups of times in guest mode. Data stored with the app is stored locally on the user's device and will be lost when uninstalling, clearing storage, or the phone being broken. For users who do not have an account with Response Time Tracker, their times data will not be collected. Moreover, Response Time Track does not accept any responsibility for data stored on local storage; such data will only be stored on local devices and will be lost when uninstalling, clearing storage, or the phone being broken. </p>
+        <p className="doc-txt">
+          For those users with a premium Response Time Tracker account, their times are collected for cloud storage and are stored on Google systems. Furthermore, these times are synchronized with cloud storage so that in cases of uninstalling, clearing storage, or a broken phone the data can be restored.
         </p>
-        <h3>Third Party Login</h3>
+        <h3>Settings</h3>
         <p className="doc-txt">
-          The app uses Firebase authentication for users who sign up with Google
-          or Apple. The account is for cloud stoage for those who pay for it.
-          Accounts can be deleted by going to the setting page of the app.
+          We do not keep track of the settings in Response Time Track. Furthermore, we can't support updating settings, as that is stored locally on your device.
         </p>
-        <h5>Data Collected</h5>
+        <h3>Users</h3>
         <p className="doc-txt">
-          Firebase analytics collects users clicks throughout the app. The
-          clicks are not identifiable with any user. Firebase crashlytics is
-          used to detect crashes from the app. Email can be shared with account
-          login to help sync the user.
+          We utilize email, Google, and Apple login capabilities to match users on multiple devices using the same credentials. Additionally, we generate a user ID to track usage and subscription information.
         </p>
-        <h5>Information Is For</h5>
+        <h5>Analytics</h5>
         <p className="doc-txt">
-          The information rescue times is used for first responders to identify
-          and analysis rescue times.
+          We use Firebase Analytics to track user interactions with the Response Time Track app, so that we can better understand how it is being used and use this information to develop new features.
         </p>
-        <h5>Information Sharing</h5>
+        <h5>Crashes</h5>
         <p className="doc-txt">
-          The information from accounts is shared with Firebase database and
-          will only be access by the user.
+          We utilize Firebase Crashlytics to keep track of crash logs users may encounter while utilizing the Response Time Track app. This information is used solely to help debug and improve the quality of the app. No user data is collected and there is no option for users to opt-out of this process.
+        </p>
+        <h5>ADs</h5>
+        <p className="doc-txt">
+          We do not collect device identifiers or seek to identify individuals for advertising purposes. We use advertising to help fund the servers and backend used in Response Time Track.
         </p>
       </main>
       <Footer></Footer>
