e277180f7f83ad97541d48e5bb5bcee82d7d81cb
Owen Bean <owenbean@Owens-MBP-98.lan>
2024-05-25 15:59:30 -0400

    Added account delete page for Google

---
+++ src/pages/google-delete.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import DeleteAccountImage from "../images/delete/delete-account-app.jpg";
+import "../style/markdown.sass";
+
+// markup
+const GoogleDeletePage = () => {
+  const [phoneSize, setPhoneSize] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setPhoneSize(window.innerWidth <= 768);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div id="main">
+      <Navbar isPhone={phoneSize}></Navbar>
+      <main>
+        <title>Google Delete Page</title>
+        <h1>How to Delete Account</h1>
+        <p>If you want to delete your account, you must delete your account within the app. Within the main window of the app, press on delete account and tap yes to delete your account and all of your data.</p>
+        <img className="delete-image" src={DeleteAccountImage} alt="Delete Account Image"></img>
+      </main>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default GoogleDeletePage;
