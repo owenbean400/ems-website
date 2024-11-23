f46ae3887d71e2abd3efbb2e815b25bfa8fcff1e
owenbean400 <owenbean400@gmail.com>
2024-11-23 15:49:43 -0500

    Added feedback API connection

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,9 @@ import DestinationImg from "../images/screenshot/destination.png"
 // markup
 const IndexPage = () => {
   const [phoneSize, setPhoneSize] = useState(false);
+  const [email, setEmail] = useState("");
+  const [comment, setComment] = useState("");
+  const [feedbackStatus, setFeedbackStatus] = useState("");
 
   useEffect(() => {
     function handleResize() {
@@ -28,6 +31,44 @@ const IndexPage = () => {
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  const handleEmailChange = (event) => {
+    const value = event.target.value;
+    setEmail(value);
+  };
+
+  const handleCommentChange = (event) => {
+    const value = event.target.value;
+    setComment(value);
+  };
+
+  const submitEmail = () => {
+
+    const url = "https://feedback-3tggii3mlq-uc.a.run.app/feedback";
+
+    const data = {
+      "email": email,
+      "comment": comment
+    }
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          setFeedbackStatus("Error sending feedback.");
+          return;
+        }
+        setFeedbackStatus("Feedback sent!");
+      })
+      .catch((error) => {
+        setFeedbackStatus("Error sending feedback.");
+      });
+  };
 
   return (
     <div id="main">
@@ -55,7 +96,7 @@ const IndexPage = () => {
           </div>
           <div className="display-container">
             <h2>Releases</h2>
-            <p><Link to="/releases">Our app keeps you updated with the newest releases, ensuring you're always equipped with the latest enhancements &rarr;</Link></p>
+            <p><Link to="/releases">Our app keeps you updated with the newest releases, ensuring you"re always equipped with the latest enhancements &rarr;</Link></p>
           </div>
           <div className="display-container">
             <h2>Download</h2>
@@ -65,15 +106,16 @@ const IndexPage = () => {
         <div className="feedback-container">
           <h2>Feedback</h2>
           <p className="feedback-text">Love to hear feedback on our app and how we can improve it</p>
+          {(feedbackStatus !== "") ? <p className="feedback-response">{feedbackStatus}</p> : undefined}
           <div>
             <p className="feedback-label">Email (Optional)</p>
-            <input className="email-input" maxlength="100"></input>
+            <input className="email-input" maxlength="100" type="email" onChange={handleEmailChange}></input>
           </div>
           <div>
             <p className="feedback-label">Comment</p>
-            <textarea className="comment-input" data-limit-rows="true" maxlength="100" rows="5"></textarea>
+            <textarea className="comment-input" data-limit-rows="true" maxlength="100" rows="5" onChange={handleCommentChange}></textarea>
           </div>
-          <button>Send Feedback</button>
+          <button onClick={() => submitEmail()}>Send Feedback</button>
         </div>
       </div>
       <Footer></Footer>
